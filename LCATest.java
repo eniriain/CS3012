3bc7616464d6effa92d54629947a559428b4eab9
eniriain <43381933+eniriain@users.noreply.github.com>
2018-10-01 12:21:21 +0100

    Tests to do

---
+++ LCATest.java
@@ -0,0 +1,29 @@
+import static org.junit.Assert.*;
+ import org.junit.Test;
+ public class LCATest {
+ 	@Test // test with no nodes
+	public void testEmpty() {
+		
+	}
+ 	@Test // test with only one node in
+	public void testSingle()
+	{
+		
+	}
+	
+	@Test // test with multiple nodes
+	public void testPopulated()
+	{
+		
+	}
+	@Test // test when left or right are not in the tree
+	public void testNotInTree()
+	{
+		
+	}
+	@Test // test when v1 and v2 are the same value
+	public void testSameValue()
+	{
+		
+	}
+}
