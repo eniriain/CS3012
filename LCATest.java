3a69c0af565d128c6b5231068d596239dbbd196b
eniriain <43381933+eniriain@users.noreply.github.com>
2018-10-01 12:50:00 +0100

    updated tests to classes

--- LCATest.java
+++ LCATest.java
@@ -1,19 +1,18 @@
-import org.junit.Test;
- //Using JUNIT FrameWork
-public class LCATest {
- 	
-	@Test // test with no nodes
+import static org.junit.Assert.*;
+ import org.junit.Test;
+ public class LCATest {
+ 	@Test // test with no nodes
 	public void testEmpty() {
-		BST<Integer> testBST = new BST<Integer>();
+ 		BST<Integer> testBST = new BST<Integer>();
 		
-		assertNull( LeastCommonAncestor(testBST.root, 7, 1));
+		assertNull( LowestCommonAncestor(testBST.root, 7, 1));
 	}
  	@Test // test with only one node in
 	public void testSingle()
 	{
 		BST<Integer> testBST = new BST<Integer>();
 		testBST.put(7);
-		assertNull(LeastCommonAncestor(testBST.root,1,3));
+		assertNull(LowestCommonAncestor(testBST.root,1,3));
 		
 	}
 	
@@ -27,6 +26,31 @@ public class LCATest {
 		test.put(19);
 		test.put(9);
 		test.put(13);
-		
 	}
-}
+	@Test // test when left or right are not in the tree
+	public void testNotInTree()
+	{
+		BST<Integer> test = new BST<Integer>();
+		LowestCommonAncestor<Integer> test = new LowestCommonAncestor<Integer>();
+		test.put(9);
+		test.put(1);
+		test.put(4);
+		test.put(19);
+		test.put(9);
+		test.put(13);
+		assertNull(LCATest(test.root,19,3));
+		assertNull(test.search(test.root,19,3));
+	}
+	@Test // test when v1 and v2 are the same value
+	public void testSameValue()
+	{
+		BST<Integer> test = new BST<Integer>();
+		test.put(9);
+		test.put(1);
+		test.put(4);
+		test.put(19);
+		test.put(9);
+		test.put(13);
+		assertSame(LowestCommonAncestor(test.root,9,9),9);
+	}
+} 
